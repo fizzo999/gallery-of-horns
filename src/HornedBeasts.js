7fb3e42dac1dc2133c604f4bfb7815fa73d11dee
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-04-01 00:10:22 -0700

    adds filter, dropdown #of horns, seperated modal and likes button

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -16,12 +16,15 @@ export class HornedBeasts extends Component {
         numberOfLikes: this.state.numberOfLikes + 1
       });
     };
+    const clickModal = () => {
+      this.props.showModal(this.props.title);
+    }
     // const popOut = () => {
     //   this.props.displayAsModal(this.props.index);
     // };
     return (
       <Card style={{ width: '25rem' }} bg='light' text='dark' border='primary'>
-        <Card.Img onClick={likeImage} variant="top" src={this.props.image_url} alt={this.props.title} title={this.props.title}/>
+        <Card.Img onClick={clickModal} variant="top" src={this.props.image_url} alt={this.props.title} title={this.props.title}/>
         <Card.Body onClick={likeImage}>
           <Card.Title><h2>{this.props.title}</h2></Card.Title>
           <Card.Subtitle className="mb-2 text-muted"><h5>Number of Horns: {this.props.horns}</h5></Card.Subtitle>
